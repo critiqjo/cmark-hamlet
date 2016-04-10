b358778eaf1097513ec439ee9cc74e08121ceaa2
critiqjo <john.ch.fr@gmail.com>
2016-04-10 16:15:39 +0530

    More spec tests

--- tests/specs.rs
+++ tests/specs.rs
@@ -25,6 +25,112 @@ fn simple() {
     assert_eq!(to_html("`foo bar`"), "<p><code>foo bar</code></p>");
     assert_eq!(to_html("**foo bar**"), "<p><strong>foo bar</strong></p>");
     assert_eq!(to_html("_foo bar_"), "<p><em>foo bar</em></p>");
+    assert_eq!(to_html("> foo\n>\n> bar"), "<blockquote><p>foo</p><p>bar</p></blockquote>");
+}
+
+#[test]
+fn code_block() {
+    assert_eq!(
+        to_html("```\n\
+                 code\n\n\
+                 block\n\
+                 ```"),
+        "<pre><code>code\n\nblock\n</code></pre>"
+    );
+    assert_eq!(
+        to_html("```sh\n\
+                 code\n\n\
+                 block\n\
+                 ```"),
+        "<pre data-lang=\"sh\"><code>code\n\nblock\n</code></pre>"
+    );
+}
+
+#[test]
+fn image() {
+    assert_eq!(
+        to_html("![](/link/to/image)"),
+        "<p><img src=\"/link/to/image\" /></p>"
+    );
+    assert_eq!(
+        to_html("![foo](/link/to/image)"),
+        "<p><img src=\"/link/to/image\" alt=\"foo\" /></p>"
+    );
+    assert_eq!(
+        to_html("![](/link/to/image \"bar\")"),
+        "<p><img src=\"/link/to/image\" title=\"bar\" /></p>"
+    );
+    assert_eq!(
+        to_html("![foo](/link/to/image \"bar\")"),
+        "<p><img src=\"/link/to/image\" alt=\"foo\" title=\"bar\" /></p>"
+    );
+    assert_eq!(
+        to_html("![foo] bar\n\n[foo]: /link/to/image \"bar\""),
+        "<p><img src=\"/link/to/image\" alt=\"foo\" title=\"bar\" /> bar</p>"
+    );
+}
+
+#[test]
+fn link() {
+    assert_eq!(
+        to_html("<file:/link/to/x>"), // absolute URI <scheme:link>
+        "<p><a href=\"file:/link/to/x\">file:/link/to/x</a></p>"
+    );
+    assert_eq!(
+        to_html("[](/link/to/x)"),
+        "<p><a href=\"/link/to/x\"></a></p>"
+    );
+    assert_eq!(
+        to_html("[foo](/link/to/x)"),
+        "<p><a href=\"/link/to/x\">foo</a></p>"
+    );
+    assert_eq!(
+        to_html("[](/link/to/x \"bar\")"),
+        "<p><a href=\"/link/to/x\" title=\"bar\"></a></p>"
+    );
+    assert_eq!(
+        to_html("[foo](/link/to/x \"bar\")"),
+        "<p><a href=\"/link/to/x\" title=\"bar\">foo</a></p>"
+    );
+    assert_eq!(
+        to_html("[foo] bar\n\n[foo]: /link/to/x \"bar\""),
+        "<p><a href=\"/link/to/x\" title=\"bar\">foo</a> bar</p>"
+    );
+}
+
+#[test]
+fn list() {
+    assert_eq!(
+        to_html("- hello\n\
+                 - world"),
+        "<ul><li>hello</li><li>world</li></ul>"
+    );
+    assert_eq!(
+        to_html("1. hello\n\
+                 2. world"),
+        "<ol><li>hello</li><li>world</li></ol>"
+    );
+    assert_eq!(
+        to_html("3. hello\n\
+                 4. world"),
+        "<ol start=\"3\"><li>hello</li><li>world</li></ol>"
+    );
+    assert_eq!(
+        to_html("- hello\
+               \n  ```\
+               \n  let x;\
+               \n  ```\
+               \n- world"),
+        "<ul><li>hello<pre><code>let x;\n</code></pre></li>\
+             <li>world</li></ul>"
+    );
+    assert_eq!(
+        to_html("- hello\n\
+               \n  goo\
+               \n- world"),
+        "<ul><li><p>hello</p><p>goo</p></li>\
+             <li><p>world</p></li></ul>" // NB: different from above!!
+    );
 }
 
 #[test]
@@ -33,6 +139,7 @@ fn table() {
         to_html("head1 | head2\n\
                  ----- | -----\n\
                  foo   | bar  "),
-        "<table><tr><th>head1 </th><th> head2</th></tr><tr><td>foo   </td><td> bar</td></tr></table>"
+        "<table><tr><th>head1 </th><th> head2</th></tr>\
+                <tr><td>foo   </td><td> bar</td></tr></table>"
     );
 }
